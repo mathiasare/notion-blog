c2242d949f30f97258640af459c29c09b6610cb9
mathiasare <are.mathias@gmail.com>
2023-06-16 01:04:24 +0300

    Finished Gallery View setup

--- personal-blog/src/app/page.tsx
+++ personal-blog/src/app/page.tsx
@@ -1,11 +1,13 @@
 import { NotionPage } from '@/components/NotionPage'
 import { ROOT_PAGE_ID } from '@/lib/config'
 import notion from '@/lib/notion'
+
+export const revalidate = 3600
+
 export default async function Home() {
 
   const pageId = ROOT_PAGE_ID
   const recordMap = await notion.getPage(pageId)
 
-  return (
-      <NotionPage recordMap={recordMap} rootPageId={ROOT_PAGE_ID} /> )
+  return <NotionPage recordMap={recordMap} rootPageId={ROOT_PAGE_ID} />
 }
